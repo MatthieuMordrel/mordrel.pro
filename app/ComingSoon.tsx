3e8814e82800dadc81c2de17f0e2a7a01c5908da
MatthieuMordrel <matthieumordrel@gmail.com>
2024-04-18 15:49:46 +0200

    <3

--- app/ComingSoon.tsx
+++ app/ComingSoon.tsx
@@ -1,29 +1,25 @@
-// components/ComingSoon.tsx
+// components/BelieveInUs.tsx
 import React from 'react';
+import {HeartIcon} from "@heroicons/react/24/solid";
 
-const ComingSoon = () => {
+
+const BelieveInUs = () => {
   return (
-    <div className="mt-10 px-6 py-4 border border-gray-200 shadow-lg rounded-lg bg-white dark:bg-gray-800">
-      <h1 className="text-2xl font-bold text-gray-800 dark:text-white">We&apos;re Launching Soon</h1>
+    <div className="mt-10 px-6 py-4 border border-pink-200 shadow-lg rounded-lg bg-white dark:bg-gray-800">
+      <div className="flex justify-center items-center">
+        <HeartIcon className="w-6 h-6 text-pink-500" />
+        <h1 className="ml-2 text-2xl font-bold text-gray-800 dark:text-white">I Believe in Us</h1>
+      </div>
       <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
-        Our website is currently under construction. We will be here soon with our new awesome site. Please hang on while we get to work !
+        Just like this future website, our journey is currently under construction.
+        I believe that with love and patience, we'll continue bulding something truly special, brick by brick.
+        I love you :)
       </p>
-      {/* <form className="mt-4 flex justify-center">
-        <input
-          type="email"
-          placeholder="Enter your email"
-          className="px-4 py-2 w-64 border border-gray-300 rounded-l-lg focus:outline-none"
-          required
-        />
-        <button
-          type="submit"
-          className="px-4 py-2 bg-blue-500 text-white rounded-r-lg hover:bg-blue-600 focus:outline-none"
-        >
-          Notify Me!
-        </button>
-      </form> */}
-    </div>
+      <div style={{ textAlign: 'center', marginTop: '20px' }}>
+        <img src="/IMG_4079.jpg" alt="My Image" style={{ maxWidth: '200px', maxHeight: '200px', display: 'block', margin: '20px auto 0' }} />
+      </div>
+      </div>
   );
 };
 
-export default ComingSoon;
+export default BelieveInUs;
