cce181a1271be3864d26fcdd09db2b3e392cad6c
MatthieuMordrel <matthieumordrel@gmail.com>
2024-04-18 15:23:06 +0200

    Bug fix apostrophe
    Fixed apostrophe

--- app/ComingSoon.tsx
+++ app/ComingSoon.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const ComingSoon = () => {
   return (
     <div className="mt-10 px-6 py-4 border border-gray-200 shadow-lg rounded-lg bg-white dark:bg-gray-800">
-      <h1 className="text-2xl font-bold text-gray-800 dark:text-white">We're Launching Soon</h1>
+      <h1 className="text-2xl font-bold text-gray-800 dark:text-white">We&apos;re Launching Soon</h1>
       <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
         Our website is currently under construction. We will be here soon with our new awesome site. Please hang on while we get to work !
       </p>
