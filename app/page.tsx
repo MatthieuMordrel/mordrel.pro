1b25e7e8ef72dcb24556c9c0e0e1fbd8f367d670
MatthieuMordrel <matthieumordrel@gmail.com>
2024-05-03 14:44:23 +0200

    Commit for deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
-import ComingSoon from './ComingSoon';  // Adjust the path as necessary
-
+import ComingSoon from "./ComingSoon"; // Adjust the path as necessary
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
@@ -37,9 +35,8 @@ export default function Home() {
           priority
         />
       </div>
-        <ComingSoon />
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-      </div>
+      <ComingSoon />
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left"></div>
     </main>
   );
 }
