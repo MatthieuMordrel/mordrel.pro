96a57508296edb2be26aa7a5803bf9bde8fcedc6
MatthieuMordrel <matthieumordrel@gmail.com>
2024-07-22 12:50:53 +0200

    ensure timeline in process page works

--- app/components/Pricing Components/Timeline.tsx
+++ app/components/Pricing Components/Timeline.tsx
@@ -11,32 +11,27 @@ const processSteps = [
   {
     icon: FileIcon,
     title: 'Request submission',
-    description: 'Fill out the contact form with details about your project and the specific task(s).',
-    borderColor: 'border-[#9FD7CA]'
+    description: 'Fill out the contact form with details about your project and the specific task(s).'
   },
   {
     icon: NotebookPen,
     title: 'Review and Approval',
-    description: 'Your request will be reviewed by our team and we will contact you to gather more details about the project.',
-    borderColor: 'border-[#81B9B6]'
+    description: 'Your request will be reviewed by our team and we will contact you to gather more details about the project.'
   },
   {
     icon: CheckIcon,
     title: 'Agreement',
-    description: 'Once an aggreement is reached about the objective, the timeline and the pricing, we get to work.',
-    borderColor: 'border-[#689CA0]'
+    description: 'Once an aggreement is reached about the objective, the timeline and the pricing, we get to work.'
   },
   {
     icon: RefreshCwIcon,
     title: 'Deployment',
-    description: 'The automation, visualisation or service is developed, set up and deployed to your systems where we ensure smooth functioning.',
-    borderColor: 'border-[#527F89]'
+    description: 'The automation, visualisation or service is developed, set up and deployed to your systems where we ensure smooth functioning.'
   },
   {
     icon: ShareIcon,
     title: 'Ongoing Monitoring and Maintenance',
-    description: 'Depending on the agreed terms, we continue monitoring and maintaining the service, or ownership is given to your team.',
-    borderColor: 'border-[#406371]'
+    description: 'Depending on the agreed terms, we continue monitoring and maintaining the service, or ownership is given to your team.'
   }
 ]
 
