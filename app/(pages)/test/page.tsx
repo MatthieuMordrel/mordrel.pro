1aee08e29758d033f658fb97674a68efd0ac43da
MatthieuMordrel <matthieumordrel@gmail.com>
2024-08-10 12:03:24 +0200

    changed web portal url

--- app/(pages)/test/page.tsx
+++ app/(pages)/test/page.tsx
@@ -9,7 +9,7 @@ const TestPage = () => {
         title="Draft File - Electricity"
         width={1000}
         height={600}
-        src="https://app.powerbi.com/view?r=eyJrIjoiMGUxOTI0NjYtMzM0Yi00OTkwLWI4MTgtM2E1NDBkMzBjNGJmIiwidCI6ImE3YTRkNGE0LWZiMDctNDgxZC1iYWNmLWYyZWEzMjEyZDk5MSIsImMiOjl9"
+        src="https://app.powerbi.com/reportEmbed?reportId=7f1fcb4e-b2ab-426d-8214-0525ca5203fa&autoAuth=true&ctid=a7a4d4a4-fb07-481d-bacf-f2ea3212d991"
         className="mx-auto"
       />
     </div>
