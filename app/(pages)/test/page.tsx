64ba6be9cf10e79c1836ae020a0c23ef12c2a474
MatthieuMordrel <matthieumordrel@gmail.com>
2024-08-10 11:17:36 +0200

    changin test page

--- app/(pages)/test/page.tsx
+++ app/(pages)/test/page.tsx
@@ -4,14 +4,14 @@ import PowerBIReportweb from './PowerBIWeb'
 const TestPage = () => {
   return (
     <div>
-      <FetchTest />
-      {/* <PowerBIReportweb
+      {/* <FetchTest /> */}
+      <PowerBIReportweb
         title="Draft File - Electricity"
         width={1000}
         height={600}
         src="https://app.powerbi.com/view?r=eyJrIjoiMGUxOTI0NjYtMzM0Yi00OTkwLWI4MTgtM2E1NDBkMzBjNGJmIiwidCI6ImE3YTRkNGE0LWZiMDctNDgxZC1iYWNmLWYyZWEzMjEyZDk5MSIsImMiOjl9"
         className="mx-auto"
-      /> */}
+      />
     </div>
   )
 }
