1a4c9e622d77906f7ce98574d59a18f482fcaaaf
MatthieuMordrel <matthieumordrel@gmail.com>
2024-09-10 23:09:14 +0200

    added a 480p video for mobiles

--- app/components/Page Components/MainPageComponents/MainVideo.tsx
+++ app/components/Page Components/MainPageComponents/MainVideo.tsx
@@ -11,8 +11,9 @@ function MainVideo({ src }: { src: string }) {
         className="absolute left-0 top-0 h-full w-full object-cover opacity-10"
         aria-label="Background video"
       >
-        <source src="output_720p_30fps_crf24_h265.mp4" type="video/mp4" />
-        <source src="output_720p_30fps_crf23_h264_max_compression.mp4" type="video/mp4" />
+        <source src="output_480p_30fps_crf28_h265_mobile_noaudio.mp4" type="video/mp4" media="(max-width: 767px)" />
+        <source src="output_720p_30fps_crf24_h265.mp4" type="video/mp4" media="(min-width: 768px)" />
+        <source src="output_720p_30fps_crf23_h264_max_compression.mp4" type="video/mp4" media="(min-width: 768px)" />
         Your browser does not support the video tag.
       </video>
       <div className="absolute bottom-0 left-0 h-28 w-full bg-gradient-to-b from-transparent to-techGrey"></div>
